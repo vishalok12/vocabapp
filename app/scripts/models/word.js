83973876cfe46a07d023a632fa08d93a69eb6441
Vishal Kumar <vishal.rgiit@gmail.com>
2014-08-29 23:06:31 +0530

    styling fixes

--- app/scripts/models/word.js
+++ app/scripts/models/word.js
@@ -1,24 +1,24 @@
 define([
-    'backbone'
+	'backbone'
 ], function (Backbone) {
-    'use strict';
+	'use strict';
 
-    var WordModel = Backbone.Model.extend({
-        defaults: {
-            remembered: false,
-            synonyms: []
-        },
-        parse: function (response) {
-            response.id = response._id;
-            response.synonyms = response.synonyms ? response.synonyms.split(',') : [];
-            return response;
-        },
-        validate: function(attributes) {
-            if (!attributes.name || !attributes.meaning) {
-                return 'name and meaning both should be passed';
-            }
-        }
-    });
+	var WordModel = Backbone.Model.extend({
+		defaults: {
+			remembered: false,
+			synonyms: []
+		},
+		parse: function (response) {
+			response.id = response._id;
+			response.synonyms = response.synonyms ? response.synonyms.split(',') : [];
+			return response;
+		},
+		validate: function(attributes) {
+			if (!attributes.name || !attributes.meaning) {
+				return 'name and meaning both should be passed';
+			}
+		}
+	});
 
-    return WordModel;
+	return WordModel;
 });
