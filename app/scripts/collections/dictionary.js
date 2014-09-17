c923af88d07ef8685d0fe56ea9068b2b6f9da3c2
Vishal Kumar <vishal.rgiit@gmail.com>
2014-09-18 01:05:32 +0530

    use local-storage to save words in client-side

--- app/scripts/collections/dictionary.js
+++ app/scripts/collections/dictionary.js
@@ -1,15 +1,17 @@
 /*global define*/
 
 define([
-    'backbone',
-    'models/word'
+	'backbone',
+	'models/word',
+	'localStorage'
 ], function (Backbone, wordModel) {
-    'use strict';
+	'use strict';
 
-    var DictionaryCollection = Backbone.Collection.extend({
-        url: 'api/words',
-        model: wordModel
-    });
+	var DictionaryCollection = Backbone.Collection.extend({
+		localStorage: new Backbone.LocalStorage('dictionary'),
+		url: 'api/words',
+		model: wordModel
+	});
 
-    return DictionaryCollection;
+	return DictionaryCollection;
 });
--- app/scripts/models/word.js
+++ app/scripts/models/word.js
@@ -10,7 +10,8 @@ define([
 		},
 		parse: function (response) {
 			response.id = response._id;
-			response.synonyms = response.synonyms ? response.synonyms.split(',') : [];
+			response.synonyms = response.synonyms ? response.synonyms instanceof Array ?
+				response.synonyms : response.synonyms.split(',') : [];
 			return response;
 		},
 		validate: function(attributes) {
