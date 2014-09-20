a31d742f4b9ceeb32a0c62c64f1a1914e0623407
Vishal Kumar <vishal.rgiit@gmail.com>
2014-09-20 09:46:41 +0530

    add cors support

--- app/scripts/views/add_word/meaning_view.js
+++ app/scripts/views/add_word/meaning_view.js
@@ -1,82 +1,82 @@
 define([
-    'jquery',
-    'backbone',
-    'templates'
+	'jquery',
+	'backbone',
+	'templates'
 ], function ($, Backbone, JST) {
-    'use strict';
+	'use strict';
 
-    var MeaningView = Backbone.View.extend({
-        tagName: 'li',
-        className: 'word-meaning',
+	var MeaningView = Backbone.View.extend({
+		tagName: 'li',
+		className: 'word-meaning',
 
-        template: JST['app/scripts/templates/add_word/meaning.ejs'],
+		template: JST['app/scripts/templates/add_word/meaning.ejs'],
 
-        events: {
-            'click .stored-value': "editMeaning",
-            'keydown .edit-value': "preventSpaceScroll",
-            'keypress .edit-value': "updateMeaning",
-            'blur .edit-value': "checkMeaning",
-            'click .trash-icon': "deleteView"
-        },
+		events: {
+			'click .stored-value': "editMeaning",
+			'keydown .edit-value': "preventSpaceScroll",
+			'keypress .edit-value': "updateMeaning",
+			'blur .edit-value': "checkMeaning",
+			'click .trash-icon': "deleteView"
+		},
 
-        initialize: function(opts) {
-            this.render(opts);
-            this.$storedValue = this.$el.find('.stored-value');
-            this.$input = this.$el.find('.edit-value');
-        },
+		initialize: function(opts) {
+			this.render(opts);
+			this.$storedValue = this.$el.find('.stored-value');
+			this.$input = this.$el.find('.edit-value');
+		},
 
-        render: function(opts) {
-            this.$el.html( this.template(opts) );
+		render: function(opts) {
+			this.$el.html( this.template(opts) );
 
-            return this;
-        },
+			return this;
+		},
 
-        getValue: function() {
-            return this.$storedValue.text().trim();
-        },
+		getValue: function() {
+			return this.$storedValue.text().trim();
+		},
 
-        editMeaning: function() {
-            this.$input.val(this.$storedValue.text());
-            this.$el.addClass('editing');
-            this.$input.select();
-        },
+		editMeaning: function() {
+		  this.$input.val(this.$storedValue.text());
+		  this.$el.addClass('editing ds-no-drag');
+		  this.$input.select();
+		},
 
-        preventSpaceScroll: function(e) {
-            // prevent scroll-down from space
-            if (e.keyCode === 32) {
-                this.$input.val(this.$input.val() + " ");
+		preventSpaceScroll: function(e) {
+			// prevent scroll-down from space
+			if (e.keyCode === 32) {
+				this.$input.val(this.$input.val() + " ");
 
-                return false;
-            }
+				return false;
+			}
 
-        },
+		},
 
-        updateMeaning: function(e) {
-            var newValue = this.$input.val() + String.fromCharCode(e.keyCode);
-            this.$storedValue.text(newValue);
+		updateMeaning: function(e) {
+			var newValue = this.$input.val() + String.fromCharCode(e.keyCode);
+			this.$storedValue.text(newValue);
 
-            if (e.keyCode === 13) {
-                this.showMeaning();
-            }
-        },
+			if (e.keyCode === 13) {
+				this.showMeaning();
+			}
+		},
 
-        showMeaning: function() {
-            this.$el.removeClass('editing');
-        },
+		showMeaning: function() {
+			this.$el.removeClass('editing ds-no-drag');
+		},
 
-        deleteView: function() {
-            this.remove();
-            this.trigger("destroy", this);
-        },
+		deleteView: function() {
+			this.remove();
+			this.trigger("destroy", this);
+		},
 
-        checkMeaning: function() {
-            if (!this.$input.val()) {
-                this.deleteView();
-            } else {
-                this.showMeaning();
-            }
-        }
-    });
+		checkMeaning: function() {
+			if (!this.$input.val()) {
+				this.deleteView();
+			} else {
+				this.showMeaning();
+			}
+		}
+	});
 
-    return MeaningView;
+	return MeaningView;
 });
